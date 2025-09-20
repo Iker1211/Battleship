09d16233632a470e219b764d6db3ea51a2059da9
iker <velezlucasiker1@gmail.com>
2025-09-20 14:08:46 -0500

    deleted private and static elements

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,30 +1,13 @@
 import Ship from "./Ship.js"
 
 export default class Gameboard {
-    static #rows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    static #columns = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
-    static #board = [];
 
-    static createGrid() {
-        for (let i = 0; i < this.#rows.length; i++) {
-            for (let j = 0; j < this.#columns.length; j++) {
-
-                let cell = {
-                    row: Gameboard.#rows[i],
-                    column: Gameboard.#columns[j],
-                };
-
-                console.log(Gameboard.#rows[i], Gameboard.#columns[j])
-                this.#board.push(cell);
-            }
-        }
+    constructor(dimensions) {
+        this.dimensions = dimensions;
+        let board = Array(dimensions).fill().map(() => Array(dimensions).fill('empty'));
     }
 
-    static logBoard() {
-        console.log(this.#board);
-    }
-
-    static placeShip(length, timesHit, sunk, y, x, orientation) {
+    placeShip(length, timesHit, sunk, y, x, orientation) {
 
         let newShip = new Ship(1, 0, false, 1, 1, "horizontal");
 
@@ -33,9 +16,10 @@ export default class Gameboard {
             return;
         }
 
-
+        console.log("arito")
     }
 }
 
-Gameboard.createGrid();
-Gameboard.logBoard();
+let board1 = new Gameboard(10);
+
+board1.placeShip(1, 0, false, 1, 1, "horizontal");
