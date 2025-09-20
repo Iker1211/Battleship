75b919639279cf1862fd878f56d3dbd28869f680
iker <velezlucasiker1@gmail.com>
2025-09-20 15:56:20 -0500

    placeShip method in gameBoard class and instances

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -2,12 +2,15 @@ import Ship from "./Ship.js"
 
 export default class Gameboard {
 
-    constructor(dimensions) {
-        this.dimensions = dimensions;
-        let board = Array(dimensions).fill().map(() => Array(dimensions).fill('empty'));
-    }
+     constructor() {
+        this.board = Array(10).fill().map(() => Array(10).fill('empty'));
+     }
 
-    placeShip(length, timesHit, sunk, y, x, orientation) {
+    placeShip(board, length, timesHit, sunk, y, x, orientation) {
+
+        if (x < 0 || x > 9 || y < 0 || y > 9) {
+            return false;
+        }
 
         let newShip = new Ship(1, 0, false, 1, 1, "horizontal");
 
@@ -16,10 +19,49 @@ export default class Gameboard {
             return;
         }
 
-        console.log("arito")
+        if (orientation === "horizontal") {
+            if (y + length -1 > 9) {
+                return false;
+            }
+
+            for (let i = 0; i < length; i++) {
+                if (board[x][y + i] !== "empty") {
+                    return false;
+                }
+            }
+        } else if (orientation === "veritcal") {
+            if (x + length -1 > 9) {
+                return false;
+            }
+
+            for(let i = 0; i < length; i++) {
+                if (board[x + i][y] !== "empty") {
+                    return false;
+                }
+            }
+        } else {
+            return false;
+        }
+
+        if (orientation == "horizontal") {
+            for (let i = 0; i < length; i++) {
+                board[x][y + i] = "ship";
+            }
+        } else {
+            for (let i = 0; i < length; i++) {
+                board[x + i][y] = "ship";
+            }
+        }
+
+        return true;
+    }
+
+    logBoard() {
+        console.log(this.board);
     }
 }
 
 let board1 = new Gameboard(10);
 
-board1.placeShip(1, 0, false, 1, 1, "horizontal");
+// board1.placeShip(1, 0, false, 1, 1, "horizontal");
+board1.logBoard();
--- src/app.test.js
+++ src/app.test.js
@@ -2,14 +2,13 @@ import Ship from "./Ship.js"
 import Gameboard from "./Gameboard.js"
 
 test('ship exists', () => {
-    const ship1 = new Ship(4, 4, true);
+    let ship1 = new Ship(1, 0, false, 1, 1, "horizontal");
     expect(ship1).toBeDefined();
-    expect(ship1.properlength).toEqual(4);
-    expect(ship1.timesHit).toEqual(4);
-    expect(ship1.sunk).toBeTruthy();
 });
 
 test('gameboard exists', () => {
     const gameboard1 = new Gameboard();
     expect(gameboard1).toBeDefined();
+    expect(gameboard1.placeShip()).toBeFalsy(); //Faltan los argumentos
+    expect(gameboard1.placeShip(gameboard1.board, 1, 0, false, 1, 1, "horizontal")).toBeTruthy();
 });
