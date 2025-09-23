f85960dbd2be243734efa8051f3bfbc0405101b9
iker <velezlucasiker1@gmail.com>
2025-09-22 23:10:41 -0500

    basic UI and respective ID in each cell of both boards

--- src/DOMhandler.js
+++ src/DOMhandler.js
@@ -1,9 +1,12 @@
-// app.style.backgroundColor = "red";
 const app = document.getElementById("app");
+app.style.backgroundColor = "red";
 
-export default function displayGameboards(board1, board2) {
+export function displayGameboards(board1, board2) {
   const objectBoards = [board1, board2];
   const boards = [];
+
+  let board1rowsCounter = 11;
+  let board2rowsCounter = 11;
 
   objectBoards.forEach(() => {
     let board = document.createElement("div");
@@ -14,18 +17,38 @@ export default function displayGameboards(board1, board2) {
   });
 
   for (let i = 0; i < board1.length; i++) {
+
+        board1rowsCounter--;
+        let columnsCounter = 64; // ASCII code 
+
     board1[i].forEach((element) => {
+
+        columnsCounter++;
+
       let cell = document.createElement("div");
       cell.innerHTML = "cell";
+      cell.setAttribute("id", "friend" + String.fromCharCode(columnsCounter) + board1rowsCounter);
       boards[0].appendChild(cell);
     });
   }
 
   for (let i = 0; i < board2.length; i++) {
+
+    board2rowsCounter--;
+    let columnsCounter = 64; // ASCII code 
+
     board2[i].forEach((element) => {
+
+        columnsCounter++;
+
       let cell = document.createElement("div");
       cell.innerHTML = "cell";
+      cell.setAttribute("id", "enemy" + String.fromCharCode(columnsCounter) + board2rowsCounter);
       boards[1].appendChild(cell);
     });
   }
+}
+
+export function displayShips(board1, board2) {
+    console.log("the boards", board1, board2);
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import "./styles.css";
 import Game from "./Game.js";
-import DOMhandler from "./DOMhandler.js";
-import displayGameboards from "./DOMhandler.js";
+import { displayGameboards, displayShips } from "./DOMhandler.js";
 
 let firstGame = new Game("human", "computer");
 
@@ -10,6 +9,14 @@ firstGame.player1.gameboard.placeShip(
   1,
   1,
   1,
+  "horizontal",
+);
+
+firstGame.player1.gameboard.placeShip(
+  firstGame.player1.gameboard.board,
+  4,
+  5,
+  5,
   "horizontal",
 );
 
@@ -26,7 +33,10 @@ displayGameboards(
   firstGame.player2.gameboard.board,
 );
 
-// displayGameboards();
+displayShips(
+  firstGame.player1.gameboard.ships,
+  firstGame.player2.gameboard.ships,    
+);
 
 // Por cada jugador un grid
 // La lógica del juego debería estar separada del control del DOM,
